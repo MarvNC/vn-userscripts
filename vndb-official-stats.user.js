a6bea538fc457685be709bcfa087611b299e24af
MarvNC <rotrobmin@gmail.com>
2023-02-28 12:32:06 -0800

    refactor the extractLangInfo function

--- vndb-official-stats.user.js
+++ vndb-official-stats.user.js
@@ -4,7 +4,7 @@
 // @homepageURL https://github.com/MarvNC/vn-userscripts
 // @match       https://vndb.org/v*
 // @grant       none
-// @version     1.13
+// @version     1.14
 // @author      Marv
 // @description Adds links and dates to the VNDB infobox.
 // ==/UserScript==
@@ -47,34 +47,39 @@ const vnIdRegex = /^\/(v\d+)/;
  * @returns {object} allLinks, allReleases
  */
 function getLangInfo(document) {
-  /**
-   * array of objects with links and dates for each language
-   */
+  const langInfo = extractLangInfo(document);
+  const allLinks = processLinks(langInfo);
+  const allReleases = processReleases(langInfo);
+  console.log(allLinks);
+  return { allLinks, allReleases };
+}
+
+/**
+ * Extracts language information from the given document.
+ * @param {DOM} document
+ * @returns {Array} langInfo
+ */
+function extractLangInfo(document) {
   const langInfo = [];
 
-  // check each language element
   [...document.querySelectorAll('.mainbox.vnreleases > details')].forEach((detail) => {
     const releases = detail.querySelectorAll('tr');
-    // get flag element as language
     const lang = detail.querySelector('summary > abbr.lang').outerHTML;
     const info = { lang, links: new Set() };
 
     for (const release of releases) {
       const grayedout = release.querySelector('b.grayedout')?.textContent ?? '';
-      // exclude unofficial/mtl
       if (
         !grayedout.match(/unofficial|patch|machine translation/) &&
         !release.querySelector('tr')?.classList?.contains('mtl') &&
         !release?.classList?.contains('mtl')
       ) {
-        // links
         const officialLinks = [...release.querySelectorAll('a')]
           .filter((elem) => elem.innerHTML.includes('Official website'))
           .map((anchor) => anchor.href);
         if (officialLinks.length > 0) {
           info.links.add(officialLinks[0]);
         }
-        // release date
         if (!info.release && release.querySelector(`abbr.icons[title="complete"]`)) {
           info.release = release.querySelector('.tc1').innerHTML;
         }
@@ -83,11 +88,18 @@ function getLangInfo(document) {
     langInfo.push(info);
   });
 
+  return langInfo;
+}
+
+/**
+ * Processes language information to extract all links.
+ * @param {Array} langInfo
+ * @returns {Map} allLinks
+ */
+function processLinks(langInfo) {
   const allLinks = new Map();
-  const allReleases = new Map();
   for (const lang of langInfo) {
     for (const link of [...lang.links.values()]) {
-      // clean and shorten link
       try {
         const url = new URL(link);
         let displayLink = url.hostname + url.pathname + url.search + url.hash;
@@ -97,8 +109,6 @@ function getLangInfo(document) {
           displayLink = displayLink.slice(0, 50) + '...';
         }
         const linkHTML = `<a href="${link}">${displayLink}</a>`;
-
-        // combine flags to one link for each unique link
         if (allLinks.has(linkHTML)) {
           allLinks.get(linkHTML).push(lang.lang);
         } else {
@@ -108,8 +118,18 @@ function getLangInfo(document) {
         console.log(link, error);
       }
     }
+  }
+  return allLinks;
+}
 
-    // combine flags to one entry for each release date
+/**
+ * Processes language information to extract all release dates.
+ * @param {Array} langInfo
+ * @returns {Map} allReleases
+ */
+function processReleases(langInfo) {
+  const allReleases = new Map();
+  for (const lang of langInfo) {
     if (lang.release) {
       if (allReleases.has(lang.release)) {
         allReleases.get(lang.release).push(lang.lang);
@@ -119,20 +139,19 @@ function getLangInfo(document) {
     }
   }
 
-  console.log(allLinks);
-  return { allLinks, allReleases };
+  return allReleases;
 }
 
 /**
- * 
+ *
  * @param {object} dataToLangFlags Map of links or data as the key mapped to values of arrays of languages
  */
-function makeHTMLTable(dataToLangFlags){
+function makeHTMLTable(dataToLangFlags) {
   let tableHTML = '';
   for (const [data, lang] of dataToLangFlags) {
     tableHTML += lang.join('') + data + '<br>';
   }
-  
+
   const tableElem = document.createElement('tr');
   if (dataToLangFlags.size > 0) {
     tableElem.innerHTML = `<td>Official Links</td><td>${tableHTML}</td>`;
